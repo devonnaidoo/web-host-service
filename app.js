a4193d0118b7703230dcdbf612c3ca79ce1fb439
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-07 12:56:34 +0200

    initialising app

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ var express = require("express");
 var path = require("path");
 var body_parser = require("body-parser");
 var node_mailer = require("nodemailer");
+
+// Initialising app
+var app = express();
+
+// Set body parser middleware
+// Middleware - Plugins that change the req or res object before it get handled by the application
+app.use(body_parser.json());
+app.use(
+  body_parser.urlencoded({
+    extended: false
+  })
+);
+
+app.listen(3000);
+console.log("Server running on port 3000");
